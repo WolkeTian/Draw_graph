860a57a3395ab3ed32fd34ccc069449eb31bb5d5
Yun Tian <sherfly.t@gmail.com>
2022-03-25 15:58:52 +0800

    Update circular_nets.R

--- CircularGraph/circular_nets.R
+++ CircularGraph/circular_nets.R
@@ -7,6 +7,7 @@ df0 <- read.csv("network_flow.csv", stringsAsFactors=FALSE)
 # chordDiagram(x = df0, annotationTrack = "grid")    # 该函数默认绘图，每次随机分配颜色
 # 增加link.visible = df0$flow < 0,为只画负的，相应只画正的
 # clolor = 'red'或 '#FF0000'等指定颜色,以及轮行指定颜色：col = df0$rowcol等
+# 后可跟透明度，根据高低强调或区分，比如#FF000080 80是透明度
 
 # 以下为自定义颜色绘图
 # 读取配色和label
